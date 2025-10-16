acab71d4a7810762ecf2146450238da0223a99a8
lxl <lxli4277@163.com>
2025-10-16 18:03:10 +0800

    Initial commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by LXL on 2025/10/16.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
